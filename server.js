655bad37248962c3bfefd14a95366fb09b7b819d
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-02-22 18:53:17 +0200

    update server

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const config = require('./config/config');
+// import external lib
+const express = require("express");
+const morgan = require("morgan");
+const colors = require("colors");
 
-dotenv.config({ path: './config/config.env' });
+// import internal modules
+const dbConnect = require("./config/db.js");
+const config = require("./config/config.js");
+const api = require("./routs/routeNavigator.js");
 
 const app = express();
 
-app.listen(config.PORT, ()=>{
-    console.log(`server running on ${config.NODE_ENV} in port ${config.PORT}`);
+// connect to mongoose
+dbConnect();
+
+// run logger only on development environment
+if (config.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+// configure routs
+app.use("/api/v1", api);
+
+const server = app.listen(config.PORT, () => {
+	console.log(
+		`server running on ${config.NODE_ENV} in port ${config.PORT}`.bgGreen.bold
+	);
+});
+
+// catch unhandled Rejection
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`${err.message}`.bgRed);
+
+	//close server
+	server.close(() => process.exit(1));
 });
