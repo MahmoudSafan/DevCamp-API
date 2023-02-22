d0d588933aed498d77353bc5d1382df5a9e29df0
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-02-22 18:52:51 +0200

    initial config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+
 module.exports = {
-    PORT : process.env.PORT || 5000,
-    NODE_ENV : process.env.NODE_ENV
-}
+	PORT: process.env.PORT || 5000,
+	NODE_ENV: process.env.NODE_ENV,
+	DB_URL: process.env.DB_URL,
+};
