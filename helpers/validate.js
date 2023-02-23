53f41e955a80e5add9688b7790243647dabb91de
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-02-24 01:31:21 +0200

    validate Objectid

---
+++ helpers/validate.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const {
+	Types: { ObjectId },
+} = mongoose;
+
+module.exports.validateObjectId = (id) => {
+	try {
+		ObjectId.isValid(id) && new ObjectId(id).toString() === id; //true or false
+	} catch (error) {
+		console.log(error.message);
+		return fasle;
+	}
+};
