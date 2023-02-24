bfb246f7f41be458d9bb32a252edd7b608171661
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-02-24 05:11:53 +0200

    remove try catch and use error handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,130 +1,78 @@
 const Bootcamp = require("../models/Bootcamp.js");
-const { validateObjectId } = require("../helpers/validate.js");
+const ErrorResponse = require("../utils/ErrorResponse.js");
+const { asyncHandler } = require("../middleware/asyncHandler.js");
 
 // @desc		return all Bootcamps
 // @route		GET /bootcamps
-const getAllBootcamps = async (req, res) => {
-	try {
-		const bootcamps = await Bootcamp.find();
+const getAllBootcamps = asyncHandler(async (req, res, next) => {
+	const bootcamps = await Bootcamp.find();
 
-		return res.status(200).json({
-			success: true,
-			count: bootcamps.length,
-			data: bootcamps,
-		});
-	} catch (error) {
-		return res.status(500).json({
-			success: false,
-		});
-	}
-};
+	return res.status(200).json({
+		success: true,
+		count: bootcamps.length,
+		data: bootcamps,
+	});
+});
 
 // @desc		return a single Bootcamp
 // @route		GET /bootcamps/:id
-const getSingleBootcamp = async (req, res) => {
-	try {
-		const id = req.params.id;
+const getSingleBootcamp = asyncHandler(async (req, res, next) => {
+	const id = req.params.id;
+	const bootcamp = await Bootcamp.findById(id);
 
-		if (!id && !validateObjectId(id))
-			throw new Error("Bootcamp's id not valid");
+	if (!bootcamp)
+		return next(new ErrorResponse(`Bootcamp is not found with id: ${id}`, 404));
 
-		const bootcamp = await Bootcamp.findById(id);
-
-		if (!bootcamp)
-			return res.status(404).json({
-				success: false,
-				message: "Bootcamp Not Founded",
-			});
-
-		return res.status(200).json({
-			success: true,
-			data: bootcamp,
-		});
-	} catch (error) {
-		return res.status(400).json({
-			success: false,
-			message: "Please enter a valid Bootcamp's id",
-		});
-	}
-};
+	return res.status(200).json({
+		success: true,
+		data: bootcamp,
+	});
+});
 
 // @desc		create new Bootcamp
 // @route		POST /bootcamps
-const createBootcamp = async (req, res) => {
-	try {
-		const bootcamp = await Bootcamp.create(req.body);
-		res.status(201).json({
-			success: true,
-			data: bootcamp,
-		});
-	} catch (error) {
-		res.status(400).json({
-			success: false,
-			message: error.message,
-		});
-	}
-};
+const createBootcamp = asyncHandler(async (req, res, next) => {
+	const bootcamp = await Bootcamp.create(req.body);
+	res.status(201).json({
+		success: true,
+		data: bootcamp,
+	});
+});
 
 // @desc		update Bootcamp
 // @route		PATCH /bootcamps/:id
-const updateBootcamp = async (req, res) => {
-	try {
-		const id = req.params.id;
+const updateBootcamp = asyncHandler(async (req, res, next) => {
+	const id = req.params.id;
 
-		if (!id && !validateObjectId(id))
-			throw new Error("Bootcamp's id not valid");
+	const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
+		new: true,
+		runValidators: true,
+	});
 
-		const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
-			new: true,
-			runValidators: true,
-		});
+	if (!bootcamp)
+		return next(new ErrorResponse(`Bootcamp is not found with id: ${id}`, 404));
 
-		if (!bootcamp)
-			return res.status(404).json({
-				success: false,
-				message: "Bootcamp not founded",
-			});
-
-		return res.status(200).json({
-			success: true,
-			data: bootcamp,
-		});
-	} catch (error) {
-		return res.status(400).json({
-			success: false,
-			message: "Bootcamp's id not valid",
-		});
-	}
-};
+	return res.status(200).json({
+		success: true,
+		data: bootcamp,
+	});
+});
 
 // @desc		delete Bootcamp
 // @route		DELETE /bootcamps/:id
-const deleteBootcamp = async (req, res) => {
-	try {
-		const id = req.params.id;
+const deleteBootcamp = asyncHandler(async (req, res, next) => {
+	const id = req.params.id;
 
-		if (!id && !validateObjectId(id))
-			throw new Error("Bootcamp's id not valid");
+	const bootcamp = await Bootcamp.findByIdAndDelete(id);
 
-		const bootcamp = await Bootcamp.findByIdAndDelete(id);
+	if (!bootcamp)
+		return next(new ErrorResponse(`Bootcamp is not found with id: ${id}`, 404));
 
-		if (!bootcamp)
-			return res.status(404).json({
-				success: false,
-				message: "Bootcamp not founded",
-			});
-
-		return res.status(200).json({
-			success: true,
-			data: {},
-		});
-	} catch (error) {
-		return res.status(400).json({
-			success: false,
-			message: "Bootcamp's id not valid",
-		});
-	}
-};
+	return res.status(200).json({
+		success: true,
+		data: {},
+	});
+});
 
 module.exports = {
 	getAllBootcamps,
