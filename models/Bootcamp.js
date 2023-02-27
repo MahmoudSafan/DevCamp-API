d27f2776b6ca9e5b878b79cbbbb8a5a9d32ef1d4
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-02-27 21:48:43 +0200

    merge bootcamps/courses routes

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -2,117 +2,123 @@ const mongoose = require("mongoose");
 const geocoder = require("../utils/GeoCoder.js");
 const slugify = require("slugify");
 
-const BootcampSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, "Please enter a name"],
-		trim: true,
-		unique: true,
-		maxlength: [50, "Name cannot be more than 50 charachter"],
-	},
-	description: {
-		type: String,
-		trim: true,
-		required: [true, "Please enter a description"],
-		maxlength: [500, "Description cannot be more than 500 charachter"],
-	},
-	email: {
-		type: String,
-		required: [true, "Please enter a mail address"],
-		match: [
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-			"Please Enter valid email address",
-		],
-	},
-	website: {
-		type: String,
-		match: [
-			/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
-			"Please enter valid url",
-		],
-	},
-	slug: {
-		type: String,
-	},
-	image: {
-		type: String,
-		default: "no_image.jpg",
-	},
-	phone: {
-		type: String,
-	},
-	address: {
-		type: String,
-		maxlength: [50, "Address cannot be more than be 50 character"],
-		required: true,
-		trime: true,
-	},
-	careers: {
-		type: [String],
-		required: [true, "Please enter a valid carrer category"],
-		enum: [
-			"Software Engineering",
-			"Web Development",
-			"Mobile Development",
-			"Information Technology",
-			"Data Science",
-			"UI/UX",
-			"Business",
-			"Other",
-		],
-	},
-	jobAssistance: {
-		type: Boolean,
-		default: false,
-	},
-	jobGuarantee: {
-		type: Boolean,
-		default: false,
-	},
-	acceptGi: {
-		type: Boolean,
-		default: false,
-	},
-	housing: {
-		type: Boolean,
-		default: false,
-	},
-	location: {
-		// GeoJSON Point
-		type: {
+const BootcampSchema = mongoose.Schema(
+	{
+		name: {
 			type: String,
-			enum: ["Point"],
-			// required: true,
+			required: [true, "Please enter a name"],
+			trim: true,
+			unique: true,
+			maxlength: [50, "Name cannot be more than 50 charachter"],
 		},
-		coordinates: {
-			type: [Number],
-			// required: true,
-			index: "2dsphere",
+		description: {
+			type: String,
+			trim: true,
+			required: [true, "Please enter a description"],
+			maxlength: [500, "Description cannot be more than 500 charachter"],
 		},
+		email: {
+			type: String,
+			required: [true, "Please enter a mail address"],
+			match: [
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+				"Please Enter valid email address",
+			],
+		},
+		website: {
+			type: String,
+			match: [
+				/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+				"Please enter valid url",
+			],
+		},
+		slug: {
+			type: String,
+		},
+		image: {
+			type: String,
+			default: "no_image.jpg",
+		},
+		phone: {
+			type: String,
+		},
+		address: {
+			type: String,
+			maxlength: [50, "Address cannot be more than be 50 character"],
+			required: true,
+			trime: true,
+		},
+		careers: {
+			type: [String],
+			required: [true, "Please enter a valid carrer category"],
+			enum: [
+				"Software Engineering",
+				"Web Development",
+				"Mobile Development",
+				"Information Technology",
+				"Data Science",
+				"UI/UX",
+				"Business",
+				"Other",
+			],
+		},
+		jobAssistance: {
+			type: Boolean,
+			default: false,
+		},
+		jobGuarantee: {
+			type: Boolean,
+			default: false,
+		},
+		acceptGi: {
+			type: Boolean,
+			default: false,
+		},
+		housing: {
+			type: Boolean,
+			default: false,
+		},
+		location: {
+			// GeoJSON Point
+			type: {
+				type: String,
+				enum: ["Point"],
+				// required: true,
+			},
+			coordinates: {
+				type: [Number],
+				// required: true,
+				index: "2dsphere",
+			},
 
-		formattedAddress: {
-			type: String,
-			// required: [true, "Please Enter an Address"],
+			formattedAddress: {
+				type: String,
+				// required: [true, "Please Enter an Address"],
+			},
+			street: String,
+			city: String,
+			state: String,
+			zipCode: String,
+			country: String,
 		},
-		street: String,
-		city: String,
-		state: String,
-		zipCode: String,
-		country: String,
+		averageRating: {
+			type: Number,
+			min: [1, "Rating must be at least 1"],
+			max: [10, "Rating cannot be more than 10"],
+		},
+		averageCost: {
+			type: Number,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
 	},
-	averageRating: {
-		type: Number,
-		min: [1, "Rating must be at least 1"],
-		max: [10, "Rating cannot be more than 10"],
-	},
-	averageCost: {
-		type: Number,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-});
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
 
 // GeoCoder & Create location
 BootcampSchema.pre("save", async function (next) {
@@ -135,6 +141,20 @@ BootcampSchema.pre("save", async function (next) {
 // create slug from Bootcamp's name
 BootcampSchema.pre("save", function (next) {
 	this.slug = slugify(this.name, { trim: true, lower: true });
+	next();
+});
+
+// reverse populate with virtuals
+BootcampSchema.virtual("courses", {
+	ref: "Course",
+	localField: "_id",
+	foreignField: "bootcamp",
+	justOne: false, // we want array not single object
+});
+
+// cascading remove courses when Bootcamp is deleted
+BootcampSchema.pre("remove", async function (next) {
+	await this.model("Course").deleteMany({ bootcamp: this._id });
 	next();
 });
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
--- routs/routeNavigator.js
+++ routs/routeNavigator.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const bootcampas = require("./bootcamps.js");
+const courses = require("./courses.js");
+
+// re-route to courses routs || get all courses in bootcamp
+router.use("/bootcamps/:bootcampId/courses", courses);
 
 router.use("/bootcamps", bootcampas);
+router.use("/courses", courses);
 
 module.exports = router;
