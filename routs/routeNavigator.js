f9556db6b46e044e50454d5f93851e280b8add82
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-03-03 23:23:08 +0200

    AUTH Router

--- routs/routeNavigator.js
+++ routs/routeNavigator.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const bootcampas = require("./bootcamps.js");
 const courses = require("./courses.js");
+const auth = require("./auth.js");
 
 // re-route to courses routs || get all courses in bootcamp
 router.use("/bootcamps/:bootcampId/courses", courses);
 
 router.use("/bootcamps", bootcampas);
 router.use("/courses", courses);
+router.use("/auth", auth);
 
 module.exports = router;
