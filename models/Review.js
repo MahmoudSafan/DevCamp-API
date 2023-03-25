9821cbbb7ad06e8e9501ec92f4cc812615ae78ec
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-03-25 03:34:10 +0200

    init Review Schema

---
+++ models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const ReviewSchema = mongoose.Schema({
+	title: {
+		type: String,
+		trim: true,
+		required: [true, "Please Enter Course Title"],
+		maxlength: [100, "Name cannot be more than 50 charachter"],
+	},
+	text: {
+		type: String,
+		trim: true,
+		required: true,
+		maxlength: [500, "comment cannot be more than 500 charachter"],
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 10,
+		required: [true, "Please add a rating between 1 and 10"],
+	},
+	bootcamp: {
+		type: mongoose.Schema.ObjectId,
+		ref: "Bootcamp",
+		required: true,
+	},
+	user: {
+		type: mongoose.Schema.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
