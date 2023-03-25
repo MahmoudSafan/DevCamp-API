6fe7071f69f7d19103bb8a6b8b3cf1b95c8f807a
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-03-25 04:02:03 +0200

    Create Review for Bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,5 @@
 const Review = require("../models/Review.js");
+const Bootcamp = require("../models/Bootcamp.js");
 const ErrorResponse = require("../utils/ErrorResponse.js");
 const { asyncHandler } = require("../middleware/asyncHandler.js");
 
@@ -19,4 +20,42 @@ exports.getAllReviews = asyncHandler(async (req, res, next) => {
 	} else {
 		return res.status(200).json(res.advancedFilter);
 	}
+});
+
+// @desc    get single review
+// @routs   GET /reviews/:reviewId
+exports.getSingleReview = asyncHandler(async (req, res, next) => {
+	const { id } = req.params;
+
+	const review = await Review.findById(id).populate({
+		path: "bootcamp",
+		select: "name location.formattedAddress",
+	});
+
+	if (!review)
+		return next(new ErrorResponse(`Review is not found with id: ${id}`, 404));
+
+	return res.status(200).json({
+		success: true,
+		data: review,
+	});
+});
+
+// @desc   create review
+// @routs  POST /bootcamps/:bootcampId/reviews
+exports.createReview = asyncHandler(async (req, res, next) => {
+	const { bootcampId } = req.params;
+	req.body.bootcamp = bootcampId;
+	req.body.user = req.user.id;
+
+	const bootcamp = await Bootcamp.findById(bootcampId);
+	if (!bootcamp)
+		return next(new ErrorResponse(`Bootcamp is not found with id: ${id}`, 404));
+
+	const review = await Review.create(req.body);
+
+	return res.status(201).json({
+		success: true,
+		data: review,
+	});
 });
--- routs/reviews.js
+++ routs/reviews.js
@@ -1,17 +1,26 @@
 const router = require("express").Router({ mergeParams: true });
 const Review = require("../models/Review.js");
-const { getAllReviews } = require("../controllers/reviews.js");
+const {
+	getAllReviews,
+	getSingleReview,
+	createReview,
+} = require("../controllers/reviews.js");
 
 //@desc		advancedFilter is middleware takes {model and populate} and do filter
 const { advancedFilter } = require("../middleware/advancedFilter");
 const { auth, accessRole } = require("../middleware/auth.js");
 
-router.route("/").get(
-	advancedFilter(Review, {
-		path: "bootcamp",
-		select: "name description location.formattedAddress",
-	}),
-	getAllReviews
-);
+// routs begin with api/v1/reviews || api/bootcamps/:bootcampId/reviews
+router
+	.route("/")
+	.get(
+		advancedFilter(Review, {
+			path: "bootcamp",
+			select: "name description location.formattedAddress",
+		}),
+		getAllReviews
+	)
+	.post(auth, accessRole("admin", "user"), createReview);
 
+router.route("/:id").get(getSingleReview);
 module.exports = router;
